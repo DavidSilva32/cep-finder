fd04c6e4d4581f2d56ecd190f3a032a071faddb4
DavidSilva32 <david.silvaxd32@gmail.com>
2023-04-18 00:22:59 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
+import { useState } from 'react';
 import { FiSearch } from "react-icons/fi";
+import api from './services/api'
+
 import "./App.sass";
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    // 01310930/json
+
+    if(input === ''){
+      alert('Fill in some zip code!\nPreencha algum cep!')
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+    }catch{
+      alert('oops error when fetching\noops erro ao buscar')
+      setInput('')
+    }
+  }
+
   return (
     <div className="container">
-      <h1 className="container-title">Zip code finder</h1>
+      <h1 className="container-title">CEP Finder</h1>
 
-      <form className="container-input" method="post">
-        <input type="text" placeholder="Enter your zip code..." />
-        <button className="container-input-searchButton">
+      <div className="container-input">
+        <input type="text" placeholder="Enter your zip code..." value={input} onChange={(e) => setInput(e.target.value)} />
+        <button className="container-input-searchButton" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
-      </form>
+      </div>
 
-      <main className="container-main">
-        <h2>Zip code: 79003222</h2>
-        <span>Rua Teste algum</span>
-        <span>Complemento: Algum complemento</span>
-        <span>Vila Rosa</span>
-        <span>Campo Grande - RS</span>
-      </main>
+      {Object.keys(cep).length > 1 && (
+        <main className="container-main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>Street: {cep.logradouro}</span>
+          <span>Complement: {cep.complemento}</span>
+          <span>Neighborhood: {cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
     </div>
   );
 }
